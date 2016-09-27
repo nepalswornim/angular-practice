dd289287031fbcf39a635ac6a8cf0228757905d4
swornim nepal <swornim nepal>
2016-09-27 15:29:46 +0545

    Full Project

---
+++ AngularCRUDMVCALL/AngularCRUDMVCALL/Controllers/HomeController.cs
@@ -0,0 +1,75 @@
+﻿using AngularCRUDMVCALL.Models;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AngularCRUDMVCALL.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Home/
+        private MVCFirstDBEntities _db = new MVCFirstDBEntities();
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public JsonResult GetAllUsers()
+        {
+            var users = _db.tbl_User.ToList();
+
+            return Json(users, JsonRequestBehavior.AllowGet);
+        }
+
+        public JsonResult AddUser(tbl_User _tbl)
+        {
+            _db.tbl_User.Add(_tbl);
+            _db.SaveChanges();
+            List<tbl_User> tlu = _db.tbl_User.ToList();
+            return Json(tlu, JsonRequestBehavior.AllowGet);
+        }
+
+        public JsonResult GetByID(int id)
+        {
+            tbl_User tbl = _db.tbl_User.Single(u => u.UserId == id);
+
+            return Json(tbl, JsonRequestBehavior.AllowGet);
+        }
+
+        [HttpPost]
+        public JsonResult UpdateUser(tbl_User _tbl)
+        {
+            _db.Entry(_tbl).State = EntityState.Modified;
+
+            _db.SaveChanges();
+            List<tbl_User> ttt = _db.tbl_User.ToList();
+
+            return Json(ttt, JsonRequestBehavior.AllowGet);
+        }
+
+        [HttpPost]
+        public JsonResult DeleteUser(int id)
+        {
+            tbl_User _oemployee = _db.tbl_User.Find(id);
+            _db.Entry(_oemployee).State = EntityState.Deleted;
+            _db.SaveChanges();
+            List<tbl_User> lstemp = _db.tbl_User.ToList();
+            return Json(lstemp, JsonRequestBehavior.AllowGet);
+        }
+
+        //[HttpPost] alternative
+        //public JsonResult UpdateEmployee(Employee _emp)
+        //{
+        //    _db.Entry(_emp).State = EntityState.Modified;
+        //    _db.SaveChanges();
+
+        //    List<Employee> lstemp = _db.Employees.ToList();
+        //    return Json(lstemp, JsonRequestBehavior.AllowGet);
+        //}
+    }
+}
---
+++ AngularCRUDMVCALL/AngularCRUDMVCALL/Scripts/app.js
@@ -0,0 +1,71 @@
+﻿// <reference path="angular.js" />
+var myApp = angular.module("myApp", []);
+myApp.controller("userCtrl", function ($scope, $http) {
+    $scope.users = "";
+    $http.get("/Home/GetAllUsers")
+    .success(function (result) {
+        $scope.users = result;
+    })
+.error(function (result) {
+    console.log(result);
+})
+
+    $scope.saveuser = function (user) {
+        debugger;
+        $http.post("/Home/AddUser", { _tbl: user })
+        .success(function (result) {
+            debugger;
+            $scope.users = result;
+
+            $scope.user = "";
+            $scope.message = "Inserted Successfully";
+        })
+        .error(function (result) {
+            console.log(result);
+        })
+    }
+
+    $scope.user = "";
+    $scope.getbyid = function (id) {
+        debugger;
+        $http.post("/Home/GetById", { id: id })
+        .success(function (result) {
+            debugger;
+            $scope.user = result;
+        })
+        .error(function (result) {
+            console.log(error)
+        })
+    }
+
+    $scope.updateuser = function (user) {
+        debugger;
+        $http.post("/Home/UpdateUser/", { _tbl: user })
+        //this is the name in the public son result tablename object
+        .success(function (result) {
+            $scope.users = result;
+
+            $scope.message = "Updated Successfully";
+        })
+        .error(function (result) {
+            console.log(result);
+        })
+    }
+
+    $scope.deleteuser = function (id) {
+        debugger;
+        varIsConf = confirm(' Are you sure Want to delete ?');
+        if (varIsConf) {
+            debugger;
+            $http.post("/Home/DeleteUser/", { id: id })
+
+            .success(function (result) {
+                $scope.users = result;
+                alert(" Deleted Successfully!!!");
+            })
+            .error(function (result) {
+                alert(result);
+            })
+        }
+    }
+});
